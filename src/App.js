5b76342e20786fd2601d41230751816e1b54d778
Daphne <dolllikewawa@gmail.com>
2018-05-25 16:45:52 -0700

    initial setup

---
+++ src/App.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import * as BooksAPI from './BooksAPI';
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import BookList from './BookList';
+import './App.css';
+import { Route } from "react-router-dom";
+
+class ReadingBooks extends Component {
+  state = {
+    books: []
+  }
+
+  componentDidMount() {
+    BooksAPI.getAll().then((books) => {
+      this.setState({
+        books
+      })
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <nav>
+            <h1>My Bookshelf</h1> 
+          </nav>
+        </header>
+        <Tabs>
+          <TabList>
+            <Tab>Reading</Tab>
+            <Tab>Want To Read</Tab>
+            <Tab>Read</Tab>
+          </TabList>
+
+          <TabPanel>
+            {/* the state of books should be same as the state of shelf */}
+            <BookList />
+          </TabPanel>
+          <TabPanel>
+            Books Going to read
+          </TabPanel>
+          <TabPanel>
+            I already read
+          </TabPanel>
+        </Tabs> 
+      </div>
+    );
+  }
+}
+
+export default ReadingBooks;
---
+++ src/BookList.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class BookList extends Component {
+    render() {
+        console.log(this.props)
+        return (
+            <section>
+                
+            </section>
+
+        )
+    }
+}
+
+
+export default BookList;
